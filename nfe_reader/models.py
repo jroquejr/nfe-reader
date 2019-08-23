1f8312ee053bd2ee4ff5a9addbd8e4e688b1b621
jroquejr <rokrjr@gmail.com>
2019-08-22 21:58:59 -0300

    model basic

--- nfe_reader/models.py
+++ nfe_reader/models.py
@@ -1,30 +1,30 @@
 from schematics.models import Model
 from schematics.types import DateTimeType, FloatType, StringType, NumberType
 
-# class EmitterModel(Model):
-#     name = StringType(required=True)
-#     cnpj = NumberType(required=True)
-#     uf = StringType(max_length=2, required=True)
-#     fantasy_name = StringType()
-#     state_reg = NumberType()
-#     address = StringType()
-#     district = StringType()
-#     zipcode = NumberType()
-#     city_code = StringType()
-#     city_name = StringType()
+class EmitterModel(Model):
+    name = StringType(required=True)
+    cnpj = NumberType(required=True)
+    uf = StringType(max_length=2, required=True)
+    fantasy_name = StringType()
+    state_reg = NumberType()
+    address = StringType()
+    district = StringType()
+    zipcode = NumberType()
+    city_code = StringType()
+    city_name = StringType()
 
 
-# class ProductModel(Model):
-#     description = StringType()
-#     quantity = FloatType()
-#     business_unity = StringType()
-#     total_value = FloatType()
-#     unit_value = FloatType()
-#     product_code = StringType()
-#     ncm_code = StringType()
-#     cfop = StringType()
-#     total_tax = FloatType()
-#     metadata = BaseType()
+class ProductModel(Model):
+    description = StringType()
+    quantity = FloatType()
+    business_unity = StringType()
+    total_value = FloatType()
+    unit_value = FloatType()
+    product_code = StringType()
+    ncm_code = StringType()
+    cfop = StringType()
+    total_tax = FloatType()
+    metadata = BaseType()
 
 
 class NFeModel(Model):
