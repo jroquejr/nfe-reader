4c010c3af67341c89dc04f8f99876b45fbcdcab6
jroquejr <rokrjr@gmail.com>
2019-08-27 01:01:34 -0300

    ignore big line

--- nfe_reader/base.py
+++ nfe_reader/base.py
@@ -10,7 +10,7 @@ class Crawler:
         self.session = Session()
         self.session.headers.update(
             {
-                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"  # noqa E501
             }
         )
 
