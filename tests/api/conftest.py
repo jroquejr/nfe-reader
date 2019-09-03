5463594308ed50e6a3e823e704ae5df0e3f3394d
Raphael Fruneaux <raphaelfruno@gmail.com>
2019-09-03 09:24:00 -0300

    trying to mock the api/crawler

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -3,11 +3,48 @@ from pytest import fixture
 from api.app import create_app
 
 
-@fixture
+@fixture(scope="module")
 def app():
     return create_app({"TESTING": True})
 
 
-@fixture
+@fixture(scope="module")
 def client(app):
     return app.test_client()
+
+
+@fixture(scope="module")
+def nf_data():
+    return {
+        "access_key": "29190734274233001508650060001843241156013508",
+        "emitter": {
+            "address": "EDISTIO PONDE, 474",
+            "city_code": "2927408",
+            "city_name": "Salvador",
+            "cnpj": 34274233001508,
+            "district": "STIEP",
+            "fantasy_name": "",
+            "name": "PETROBRAS DISTRIBUIDORA S A",
+            "state_reg": 57489368,
+            "uf": "BA",
+            "zipcode": 41770395,
+        },
+        "issue_date": "2019-04-07T18:43:47.000000-0300",
+        "number": "184324",
+        "products": [
+            {
+                "business_unity": "L",
+                "cfop": "5656",
+                "description": "GASOLINA COMUM C",
+                "metadata": {"code_anp": "320102001", "uf": "BA"},
+                "ncm_code": "27101259",
+                "product_code": "0000000010101",
+                "quantity": 11.313,
+                "total_tax": 22.72,
+                "total_value": 50.0,
+                "unit_value": 4.42,
+            }
+        ],
+        "protocol": None,
+        "total_value": 50.0,
+    }
