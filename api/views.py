7f29345ef5dc5a1592ac5da16e2fb50d0a61ef68
jroquejr <rokrjr@gmail.com>
2019-09-06 21:53:05 -0300

    form

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Blueprint, request
+from flask import Blueprint, render_template, request
 
 from nfe_reader.ba.crawler import Crawler
 
@@ -10,6 +10,11 @@ blueprint = Blueprint("api", __name__)
 @blueprint.route("/healthcheck", methods=("GET",))
 def healthcheck():
     return {"status": True, "now": datetime.now()}
+
+
+@blueprint.route("/search", methods=("GET",))
+def nfe_search():
+    return render_template("form.html")
 
 
 @blueprint.route("/", methods=("POST",))
@@ -23,7 +28,8 @@ def nfe_reader():
     try:
         crawler = Crawler()
         result = crawler.search_by_qrcode(url_qrcode)
-    except Exception:
-        return {"message": "Couldnt read the URL"}, 500
+    except Exception as e:
+        print(e)
+        return {"message": f"Couldnt read the URL"}, 500
 
     return {"data": result.to_primitive()}
